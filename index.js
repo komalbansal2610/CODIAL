6eca80794a20de5723a45176870d122aa3e8b7b9
komalbansal2610 <bansalkomal2610@gmail.com>
2023-07-29 15:00:51 +0530

    set the  view engine and view property of app

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app=express();
 const port=8000;
 
 app.use('/',require('./routes'));
+app.set('viewengine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(error)
 {
     if(error)
